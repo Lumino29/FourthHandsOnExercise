1596ca87e857ae6d29aaf0347e51a72c2c3ee5ee
mrojerodev <mrojerodev@gmail.com>
2025-04-18 19:47:34 +0800

    With Validation

--- app/src/main/java/com/example/fourthhandsonexercise/MainActivity.java
+++ app/src/main/java/com/example/fourthhandsonexercise/MainActivity.java
@@ -52,50 +52,52 @@ public class MainActivity extends AppCompatActivity {
                 builder.setIcon(android.R.drawable.ic_dialog_alert);
                 builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
-                        String remark = "";
-                        double  equiv ;
-                        String name = etName.getText().toString();
-                        String prelim = etPrelim.getText().toString();
-                        String midterm = etMidterm.getText().toString();
-                        String finalGrade = etFinal.getText().toString();
-                        double prelimGrade = Double.parseDouble(prelim);
-                        double midtermGrade = Double.parseDouble(midterm);
-                        double finalGradeGrade = Double.parseDouble(finalGrade);
-                        double semGrade = (prelimGrade + midtermGrade + finalGradeGrade)/3;
-                        tvStudentName.setText(name);
-                        tvSemGrade.setText(String.valueOf(semGrade));
-                        if(semGrade == 100){
-                            equiv = 1.00;
-                            remark = "PASSED";
-                        }else if(semGrade >= 95 && semGrade <= 99){
-                            equiv = 1.50;
-                            remark = "PASSED";
-                        }else if(semGrade >= 90 && semGrade <= 94){
-                            equiv = 2.00;
-                            remark = "PASSED";
-                        }else if(semGrade >= 85 && semGrade <= 89){
-                            equiv = 2.50;
-                            remark = "PASSED";
-                        }else if(semGrade >= 80 && semGrade <= 84){
-                            equiv = 3.00;
-                            remark = "PASSED";
-                        }else if(semGrade >= 75 && semGrade <= 79){
-                            equiv = 3.50;
-                            remark = "PASSED";
-                        }else {
-                            equiv = 5.00;
-                            remark = "FAILED";
-                        }
+                        if(!checkError()) {
+                            String remark = "";
+                            double equiv;
+                            String name = etName.getText().toString();
+                            String prelim = etPrelim.getText().toString();
+                            String midterm = etMidterm.getText().toString();
+                            String finalGrade = etFinal.getText().toString();
+                            double prelimGrade = Double.parseDouble(prelim);
+                            double midtermGrade = Double.parseDouble(midterm);
+                            double finalGradeGrade = Double.parseDouble(finalGrade);
+                            double semGrade = (prelimGrade + midtermGrade + finalGradeGrade) / 3;
+                            tvStudentName.setText(name);
+                            tvSemGrade.setText(String.valueOf(semGrade));
+                            if (semGrade == 100) {
+                                equiv = 1.00;
+                                remark = "PASSED";
+                            } else if (semGrade >= 95 && semGrade <= 99) {
+                                equiv = 1.50;
+                                remark = "PASSED";
+                            } else if (semGrade >= 90 && semGrade <= 94) {
+                                equiv = 2.00;
+                                remark = "PASSED";
+                            } else if (semGrade >= 85 && semGrade <= 89) {
+                                equiv = 2.50;
+                                remark = "PASSED";
+                            } else if (semGrade >= 80 && semGrade <= 84) {
+                                equiv = 3.00;
+                                remark = "PASSED";
+                            } else if (semGrade >= 75 && semGrade <= 79) {
+                                equiv = 3.50;
+                                remark = "PASSED";
+                            } else {
+                                equiv = 5.00;
+                                remark = "FAILED";
+                            }
 
-                        if(remark.equals("PASSED")){
-                            tvRemark.setTextColor(Color.GREEN);
-                        }else{
-                            tvRemark.setTextColor(Color.RED);
+                            if (remark.equals("PASSED")) {
+                                tvRemark.setTextColor(Color.GREEN);
+                            } else {
+                                tvRemark.setTextColor(Color.RED);
+                            }
+                            tvEquiv.setText(String.valueOf(equiv));
+                            tvRemark.setText(remark);
+                            etName.requestFocus();
+                            dialog.dismiss();
                         }
-                        tvEquiv.setText(String.valueOf(equiv));
-                        tvRemark.setText(remark);
-                        etName.requestFocus();
-                        dialog.dismiss();
                     }
                 });
                 builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
@@ -141,5 +143,40 @@ public class MainActivity extends AppCompatActivity {
 
     };
 
+    public boolean checkError(){
+        EditText[] etFields = new EditText[] {etName, etPrelim, etMidterm, etFinal};
+        EditText[] etNumericFields = new EditText[] {etPrelim, etMidterm, etFinal};
+        boolean emptyError = false;
+        boolean alphaError = false;
+        boolean numericError = false;
 
+        for(EditText etField : etFields){
+            if(etField.getText().toString().trim().isEmpty()){
+                etField.requestFocus();
+                etField.setError("Field Required");
+                emptyError = true;
+            }
+        }
+
+        if(etName.getText().toString().trim().matches("[^a-zA-Z\\s]+")){
+            etName.requestFocus();
+            etName.setError("Letters and Spaces only");
+            alphaError = true;
+        }
+
+        for(EditText etNumericField : etNumericFields){
+            String fieldInput = etNumericField.getText().toString();
+            if(!fieldInput.isEmpty()){
+                try{
+                    Double.parseDouble(fieldInput);
+                } catch (NumberFormatException e) {
+                    etNumericField.requestFocus();
+                    etNumericField.setError("Enter a valid number");
+                    numericError = true;
+                }
+            }
+        }
+
+        return emptyError || alphaError || numericError;
+    }
 }
